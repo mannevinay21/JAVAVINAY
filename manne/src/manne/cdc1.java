9cfcd616e957b4c4b0c4ce1840d92fe1280de840
mannevinay21 <vinaymanne528@gmail.com>
2025-05-21 17:51:46 +0530

    Add files via upload

---
+++ manne/src/manne/cdc1.java
@@ -0,0 +1,10 @@
+package manne;
+import vinay.cd2;
+public class cdc1{
+	public static void main(String[]args)
+	{
+		cd2 c=new cd2();
+		c.add();
+	}
+
+}
---
+++ manne1/src/manne1/javap1.java
@@ -0,0 +1,9 @@
+package manne1;
+import vinay.cd2;
+
+public class javap1{
+    public static void main(String[] args) {
+    cd2 c1= new cd2(); // Creating object of cd2 class from vinay package
+        c1.add();           // Calling add() method
+    }
+}
